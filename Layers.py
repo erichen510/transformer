eeaa439dfae3f8e575dfb08f26f65a505165769a
erichen510 <1615010158@qq.com>
2019-06-24 14:23:04 +0800

    Add files via upload

--- Layers.py
+++ Layers.py
@@ -13,12 +13,12 @@ class EncoderLayer(nn.Module):
             n_head, d_model, d_k, d_v, dropout=dropout)
         self.pos_ffn = PositionwiseFeedForward(d_model, d_inner, dropout=dropout)
 
-    def forward(self, enc_input, non_pad_mask=None, slf_attn_mask=None):
+    def forward(self, enc_input, non_pad_mask=None, slf_attn_mask=None): #仅仅需要self-attention mask
         enc_output, enc_slf_attn = self.slf_attn(
             enc_input, enc_input, enc_input, mask=slf_attn_mask)
-        enc_output *= non_pad_mask
+        # enc_output *= non_pad_mask
 
         enc_output = self.pos_ffn(enc_output)
-        enc_output *= non_pad_mask
+        # enc_output *= non_pad_mask
 
         return enc_output, enc_slf_attn
